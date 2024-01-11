f9d30735f3c86d35794ead2d9e239affb5886a2e
PrasanthAtchuta <prasanth.a@piersoft.com>
2024-01-11 16:12:50 +0530

    Igonre

--- Prasanth.js
+++
@@ -1,5 +0,0 @@
-console.log("prasanthdev");
-console.log("Devansh");
-// console.log("Durga");
-console.log("Nani");
-console.log("Durgatest");
--- Prasanth1.js
+++
@@ -1,5 +0,0 @@
-console.log("Prasanth1");
-console.log("Durga");
-console.log("Devansh");
-console.log("New line");
-console.log("New line2");
